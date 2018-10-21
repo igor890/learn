869c5b4fda29bc35a28d2c194097c1d68d670f6c
Igor <igor890@ukr.net>
2018-10-21 16:25:56 +0300

    ll,l,l

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 14.10.18
+ * Time: 18:33
+ */
+
+    $num1 = 100;
+    $num2 = 20;
+
+        $res;
+
+       $res = $num1 + $num2;
+
+ echo "Итог: $res";
+?>
